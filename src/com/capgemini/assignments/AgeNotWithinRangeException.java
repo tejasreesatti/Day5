920b784ac02712e6a38a1c5b7a10125479d1473c
Satti <tejasree.satti@capgemini.com>
2018-08-31 06:04:14 -0400

    first commit

---
+++ src/com/capgemini/assignments/AgeNotWithinRangeException.java
@@ -0,0 +1,10 @@
+package com.capgemini.assignments.day5;
+
+public class AgeNotWithinRangeException extends Exception {
+
+	public AgeNotWithinRangeException(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
+}
